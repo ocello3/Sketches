b3712fb968201c004a2a96053d29d399ce0f1f95
ocello3 <tkaa.2160@gmail.com>
2020-10-05 23:42:33 +0900

    ongoing/snake

--- 20200501/src/calc.js
+++ 20200501/src/calc.js
@@ -1,10 +1,74 @@
 import P5 from 'p5';
 
 export const getParams = (windowSize) => {
+
+	const calcMargin = (canvasSize) => {
+		const x = (window.innerWidth - canvasSize) / 2;
+		const y = (window.innerHeight - canvasSize) / 2;
+		return new P5.Vector(x, y);
+	};
 	
 	const params = {};
 	params.canvasSize = (windowSize < 500) ? windowSize : windowSize * 0.6;
-	params.margin = new P5.Vector((window.innerWidth - params.canvasSize) / 2, (window.innerHeight - params.canvasSize) / 2);
+	params.margin = calcMargin(params.canvasSize);
+	params.snakeCount = 5;
+	params.pointCount = 18;
+	params.waveCount = 3;
+	params.waveLength = 6/8 * params.canvasSize;
+	params.waveLengthShrinkRate = 0.7;
+	params.totalLength = params.waveLength * params.waveCount;
+	params.pointInterval = params.totalLength / params.pointCount;
+	params.angleInterval = Math.PI * 2 * params.waveCount / params.pointCount;
+	params.waveAmp = 6/8 * params.canvasSize;
+	params.waveAmpReducRate = 0.8;
+	params.easingFactorMax = 0.4;
+	params.easingFactorReducRate = 0.65;
 	return params;
+};
+
+export const initSnake = (index) => (params) => {
+
+	const initStretchedPos = (posIndex, params) => {
+		const xHeadPos = params.totalLength;
+		const xPos = xHeadPos - params.pointInterval * posIndex;
+		const yHeadPos = params.canvasSize / (params.snakeCount + 1) * (posIndex + 1);
+		const currentWaveAmp = params.waveAmp * Math.pow(params.waveAmpReducRate, posIndex);
+		const yPos = yHeadPos + Math.sin(params.angleInterval * posIndex) * currentWaveAmp;
+		return new P5.Vector(xPos, yPos);
+	};
+
+	const initStretchedPosArray = (params) => {
+		const funcArray = Array.from(Array(params.pointCount), (pos, posIndex) => initStretchedPos(posIndex));
+		return funcArray.map(func => func(params));
+	};
+
+	const initSnake = {};
+	initSnake.status = 'keep';
+	initSnake.targetPosArray = initStretchedPosArray(index, params);
+	initSnake.currentPosArray = initStretchedPosArray(index, params);
+	return initSnake;
+};
+
+export const updateSnake = (snake, index) => (params, frameCount) => {
+
+	const updateStatus = (frameCount) => {
+		if ((frameCount + 50) % 100 == 0) return 'stretch';
+		if ((frameCount % 100)== 0) return 'shrink';
+		return 'keep'
+	};
+
+	const calcStretchedPos = (pos, posIndex) => (params) => {
+		const xIncrement = params.pointInterval * params.waveLengthShrinkRate + (params.pointCount - posIndex - 1);
+		return P5.Vector.add(pos, new P5.Vector(xIncrement, 0));
+	};
+
+	const calcShrinkPos = (pos, posIndex) => (params) => {
+		const xIncrement = params.pointInterval * params.waveLengthShrinkRate * posIndex;
+		return P5.Vector.add(pos, new P5.Vector(xIncrement, 0));
+	};
+
+	const updateSnake = {};
+	updateSnake.status = updateStatus(frameCount);
+	return updateSnake;
 };
 
