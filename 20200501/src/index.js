1f26a74e0d3460c5227d5011f284d837ab6bc8a3
ocello3 <tkaa.2160@gmail.com>
2020-10-09 18:01:17 +0900

    complete draft for 20200501

--- 20200501/src/index.js
+++ 20200501/src/index.js
@@ -8,6 +8,12 @@ const sketch = (s) => {
 	// const pane = new Tweakpane({ container:paneId });
 	const windowSize = (window.innerWidth < window.innerHeight) ? window.innerWidth : window.innerHeight;
 	const params = calc.getParams(windowSize);
+	const colorPalette = {
+		color_1: s.color('blue'),
+		color_2: s.color('red'),
+	};
+	let snakes = Array.from(Array(params.snakeCount), (snake, index) => calc.initSnake(index));
+	snakes = snakes.map(func => func(params));
 
 	s.setup = () => {
 		s.createCanvas(params.canvasSize, params.canvasSize);
@@ -16,11 +22,29 @@ const sketch = (s) => {
 	};
 
 	s.draw = () => {
+		snakes = snakes.map((snake) => calc.updateSnake(snake));
+		snakes = snakes.map(func => func(params));
 		// draw background
 		s.background(255);
 		// draw frame
 		s.fill(0);
 		s.rect(0, 0, params.canvasSize, params.canvasSize);
+		// prepare draw snake function
+		const drawSnake = (snake, index, self) => {
+			const lerpVal = index / (self.length - 1);
+			const snakeColor = s.lerpColor(params.colorPalette.color_1, params.colorPalette.color_2, lerpVal);
+			s.push();
+			s.fill(snakeColor);
+			s.beginShape();
+			snake.currentVecArray.forEach(vec => { s.curveVertex(vec.x, vec.y); });
+			s.endShape(s.CLOSE);
+			s.pop();
+		};
+		// draw snake
+		s.push();
+		s.noStroke();
+		s.fill(0);
+		snakes.forEach((snake, index, self) => drawSnake(snake, index, self));
 	};
 };
 
