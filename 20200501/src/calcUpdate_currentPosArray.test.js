f67074822be0daa385274dc08a009cc2a905cce0
ocello3 <tkaa.2160@gmail.com>
2020-10-17 20:42:51 +0900

    20200501 added tests

--- 20200501/src/calcUpdate_currentPosArray.test.js
+++ 20200501/src/calcUpdate_currentPosArray.test.js
@@ -7,21 +7,21 @@ const pointNum = params.waveNum * 4 + 1;
 
 test('calcCurrentPos', () => {
 	const currentCurrentPos = new P5.Vector(0, 0);
-	const targetPosArray = Array.from(Array(pointNum), () => new P5.Vector(0, 0));
+	const targetPosArray = Array.from(Array(pointNum), () => new P5.Vector(10, 0));
 	for (let pointIndex = 0; pointIndex < pointNum; pointIndex++) {
 		const currentPosFunc = target.calcCurrentPos(currentCurrentPos, pointIndex);
 		const currentPos = currentPosFunc(params, targetPosArray);
-		expect(currentPos.x).toBe(0);
+		expect(currentPos.x).toBeGreaterThan(0);
 		expect(currentPos.y).toBe(0);
 	}
 });
 
 test('calcCurrentPosArray', () => {
 	const currentCurrentPosArray = Array.from(Array(pointNum), () => new P5.Vector(0, 0));
-	const targetPosArray = Array.from(Array(pointNum), () => new P5.Vector(0, 0));
+	const targetPosArray = Array.from(Array(pointNum), () => new P5.Vector(10, 0));
 	const currentPosArray = target.calcCurrentPosArray(currentCurrentPosArray, params, targetPosArray);
 	currentPosArray.forEach(currentPos => {
-		expect(currentPos.x).toBe(0);
+		expect(currentPos.x).toBeGreaterThan(0);
 		expect(currentPos.y).toBe(0);
 	});
 });
