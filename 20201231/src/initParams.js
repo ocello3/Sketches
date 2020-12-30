070c1e3ade680631e0c1b4713527561612c6247a
ocello3 <tkaa.2160@gmail.com>
2020-12-30 16:20:03 +0900

    create-p5-app downloaded

---
+++ 20201231/src/initParams.js
@@ -0,0 +1,19 @@
+'use strict';
+
+export const initParams = (innerWidth, innerHeight) => {
+	
+	const windowSize = (innerWidth, innerHeight) => {
+		return (innerWidth < innerHeight) ? innerWidth : innerHeight;
+	};
+
+	const canvasSize = (windowSize) => {
+		return (windowSize < 500) ? windowSize : Math.round(windowSize * 0.6);
+	};
+
+	const params = {};
+	params.windowSize = windowSize(innerWidth, innerHeight);
+	params.canvasSize = canvasSize(params.windowSize);
+	
+	return params;
+};
+
--- create-p5js-app.sh
+++ create-p5js-app.sh
@@ -1,3 +1,13 @@
-#!/bin/zsh$
+#!/bin/zsh
 
+sed -i -e "50,50s:<tbody>:
+ <tbody>\n        <tr>\n          <td>$1</td>\n          <td><a href="20201130/pub/app.html">$2</a></td>\n          <td><a href="https://github.com/ocello3/Sketches/tree/master/$1/src">GitHub</a></td>\n        </tr>\n      </tbody>\n      <tbody>" index.html
+# wait
+# cp -r template $1
+# wait
+# cd $1
+# wait
+# npm run preinstall
+# wait
+# npm install
 
