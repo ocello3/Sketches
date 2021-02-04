225007a614bea2fbe9ebd56eb4926872c4e4306b
ocello3 <tkaa.2160@gmail.com>
2021-02-04 20:24:45 +0900

    20201231 add tonejs and confirm

--- 20201231/src/index.js
+++ 20201231/src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import P5 from 'p5';
+import * as Tone from 'tone';
 import { initParams } from './initParams.js';
 
 const sketch = (s) => {
@@ -19,11 +20,21 @@ const sketch = (s) => {
 	s.setup = () => {
 		s.createCanvas(params.canvasSize, params.canvasSize);
 		s.noLoop();
+		const isStart = confirm("Turn sound on?"); // after selected, return boolean value
+		s.noLoop();
+		if (isStart) {
+			Tone.start();
+			s.loop();
+		} else {
+			s.loop();
+			Tone.Master.mute = true;
+		}
 	};
 
 	s.draw = () => {
 		s.background(255);
 		drawFrame(params);
+		s.text(s.frameCount, s.width/2, s.height/2)
 	};
 };
 
