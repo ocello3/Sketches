8d5791ab098275a4f89bdaadf8e5942b06266451
ocello3 <tkaa.2160@gmail.com>
2021-02-18 20:46:50 +0900

    back to home cannot be changed

--- createSketchPage.js
+++ createSketchPage.js
@@ -6,16 +6,17 @@ export const getCreateP5 = (props, p5map) => {
 		props.get('coverPage').remove();
 
 		const container = s.createDiv().class('container');
-		const row = s.createDiv().class('row').parent(container);
-		s.createDiv().style('margin-top: 12%').class('one-half column').id('canvas').parent(row);
-		s.createDiv().style('margin-top: 12%').class('one-half column').id('pane').parent(row);
+		const sketchRow = s.createDiv().class('row').parent(container);
+		s.createDiv().style('margin-top: 12%').class('one-half column').id('canvas').parent(sketchRow);
+		s.createDiv().style('margin-top: 12%').class('one-half column').id('pane').parent(sketchRow);
 
 		props.set('pane', new Tweakpane({
 			container: document.getElementById('pane'),
 		}));
 		props.set('p5_20210201', new P5(p5map.get('sketch')(props), 'canvas'));
-
-		s.createDiv('back to top').parent(container).style('line-height: 2').style('color', '#1EAEDB').style('text-decoration', 'underline').style('cursor', 'pointer').mousePressed(backToTop);
+		
+		const buttonRow = s.createDiv.class('row').parent(container);
+		s.createElement('h1', 'back to top').parent(buttonRow).style('color', '#1EAEDB').style('text-decoration', 'underline').style('cursor', 'pointer').mousePressed(backToTop);
 
 		function backToTop () {
 			props.get('pane').dispose();
