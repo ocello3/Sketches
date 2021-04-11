e97bd9222030a13e1e29d2a4dead710363f9d9e9
ocello3 <tkaa.2160@gmail.com>
2021-04-11 18:16:50 +0900

    publish

--- src/getP5maps.ts
+++ src/getP5maps.ts
@@ -1,4 +1,4 @@
-import { p5_20200501 } from './20200501/p5_20200501';
+// import { p5_20200501 } from './20200501/p5_20200501';
 import { p5_20200912 } from './20200912/p5_20200912';
 import { p5_20201023 } from './20201023/p5_20201023';
 import { p5_20201231 } from './20201231/p5_20201231';
@@ -11,7 +11,7 @@ export const getP5maps = ():p5map[] => {
 	p5maps.push(p5_20201231());
 	p5maps.push(p5_20201023());
 	p5maps.push(p5_20200912());
-	p5maps.push(p5_20200501());
+	//	p5maps.push(p5_20200501());
 	return p5maps;
 }
 
